e1260b48c50b8a606be8f0f5dfbd213e379eecbc
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-07-11 08:54:50 +0545

    feat: add channels
    Channels instead of stream handles performed well for grpc.

--- console-chat/client/client.go
+++ console-chat/client/client.go
@@ -1,7 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+	"wesionary.team/dipeshdulal/console-chat/chatpb"
+)
+
+var channelName = flag.String("channel", "default", "Channel name for chatting")
+var senderName = flag.String("sender", "default", "Senders name")
+
+func joinChannel(ctx context.Context, client chatpb.ChatServiceClient) {
+
+	channel := chatpb.Channel{Name: *channelName, SendersName: *senderName}
+	stream, err := client.JoinChannel(ctx, &channel)
+	if err != nil {
+		log.Fatalf("client.JoinChannel(ctx, &channel) throws: %v", err)
+	}
+
+	fmt.Printf("Joined channel: %v \n", *channelName)
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
+			}
+
+			fmt.Printf("MESSAGE: (%v) -> %v \n", in.Sender, in.Message)
+		}
+	}()
+
+	<-waitc
+}
+
+func sendMessage(ctx context.Context, client chatpb.ChatServiceClient, message string) {
+	stream, err := client.SendMessage(ctx)
+	if err != nil {
+		log.Printf("Cannot send message: error: %v", err)
+	}
+	msg := chatpb.Message{
+		Channel: &chatpb.Channel{
+			Name:        *channelName,
+			SendersName: *senderName},
+		Message: message,
+		Sender:  *senderName,
+	}
+	stream.Send(&msg)
+
+	ack, err := stream.CloseAndRecv()
+	fmt.Printf("Message sent: %v \n", ack)
+}
 
 func main() {
+
+	flag.Parse()
+
 	fmt.Println("--- CLIENT APP ---")
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
+
+	conn, err := grpc.Dial(":5400", opts...)
+	if err != nil {
+		log.Fatalf("Fail to dail: %v", err)
+	}
+
+	defer conn.Close()
+
+	ctx := context.Background()
+	client := chatpb.NewChatServiceClient(conn)
+
+	go joinChannel(ctx, client)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		go sendMessage(ctx, client, scanner.Text())
+	}
+
 }
--- console-chat/server/server.go
+++ console-chat/server/server.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+	"wesionary.team/dipeshdulal/console-chat/chatpb"
+)
+
+type chatServiceServer struct {
+	chatpb.UnimplementedChatServiceServer
+	mu      sync.Mutex
+	channel map[string][]chan *chatpb.Message
+}
+
+func (s *chatServiceServer) JoinChannel(ch *chatpb.Channel, msgStream chatpb.ChatService_JoinChannelServer) error {
+
+	msgChannel := make(chan *chatpb.Message)
+	s.channel[ch.Name] = append(s.channel[ch.Name], msgChannel)
+
+	// doing this never closes the stream
+	for {
+		msg := <-msgChannel
+		fmt.Printf("GO ROUTINE (got message): %v \n", msg)
+		msgStream.Send(msg)
+	}
+}
+
+func (s *chatServiceServer) SendMessage(msgStream chatpb.ChatService_SendMessageServer) error {
+	msg, err := msgStream.Recv()
+
+	if err == io.EOF {
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Received Msg: %v \n", msg)
+
+	ack := chatpb.MessageAck{Status: "SENT"}
+	msgStream.SendAndClose(&ack)
+
+	go func() {
+		streams := s.channel[msg.Channel.Name]
+		for idx, msgChan := range streams {
+			fmt.Println("idx: ", idx)
+			msgChan <- msg
+			fmt.Println("After here")
+		}
+	}()
+
+	return nil
+}
+
+func newServer() *chatServiceServer {
+	s := &chatServiceServer{
+		channel: make(map[string][]chan *chatpb.Message),
+	}
+	fmt.Println(s)
+	return s
+}
 
 func main() {
 	fmt.Println("--- SERVER APP ---")
+	lis, err := net.Listen("tcp", "localhost:5400")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	chatpb.RegisterChatServiceServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
